49304aa1b69295f955113aa7d71d930481fb4dcb
valek710 <valek190194@gmail.com>
2019-01-23 20:10:09 +0200

    firstCommitUpd

--- Lesson1/src/FluorescentLights.java
+++ Lesson1/src/FluorescentLights.java
@@ -1,19 +1,29 @@
 public class FluorescentLights {
-    String manufacturer;
-    float serviceLife;
-    String type; //тип лампы (газоразрядная ртутная высокого/низкого давления, плазменный дисплей)
+    String fluorescentLightsManufacturer;
+    float fluorescentLightsServiceLife;
+    String fluorescentLightsType; //тип лампы (газоразрядная ртутная высокого/низкого давления, плазменный дисплей)
     int luminousEfficiency; //светоотдача
-    float thePrice;
+    float fluorescentLightsPrice;
     boolean surgeProtection; //наличие механизма защиты от перепадов напряжения
+    int lightBrightness;
 
-    void turnOn(){
-
+    public void turnOn(){
+        //увеличивает на 1 значение атрибута lightBrightness
     }
-    void turnOff(){
-
+    public void turnOff(){
+        int lightBrightness;
+        lightBrightness=0;
     }
-    void burnOut(){
-        //перегореть
+    public void overvoltage(){
+        //увеличивает на 5 значение атрибута lightBrightness
     }
-
+    private void switchTheLampOnAndOffOften(){
+        //уменьшает на 1 значение атрибута fluorescentLightsServiceLife
+    }
+    private void wipeTheLamp(){
+        //увеличивает на 5 значение атрибута luminousEfficiency
+    }
+    private void doNotWipeLampEveryLong(){
+        //уменьшает на 5 значение атрибута luminousEfficiency
+    }
 }
